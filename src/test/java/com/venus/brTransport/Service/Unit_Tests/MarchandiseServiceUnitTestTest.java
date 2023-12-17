bc12cfc7c6e18cb0dae078f49f62b136d4c22711
OUSSAMA HRITA <hrita.oussama@ensam-casa.ma>
2023-12-18 00:14:52 +0100

    Adding Junit and Mockito

---
+++ src/test/java/com/venus/brTransport/Service/Unit_Tests/MarchandiseServiceUnitTestTest.java
@@ -0,0 +1,9 @@
+package com.venus.brTransport.Service.Unit_Tests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MarchandiseServiceUnitTestTest {
+
+
+}
