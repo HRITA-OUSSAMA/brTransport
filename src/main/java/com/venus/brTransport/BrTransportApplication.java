75eaacfc1a40fa25486b1479e8c81ed122f40be2
OUSSAMA HRITA <hrita.oussama@ensam-casa.ma>
2023-12-18 15:59:52 +0100

    Delete pdf file

--- src/main/java/com/venus/brTransport/BrTransportApplication.java
+++ src/main/java/com/venus/brTransport/BrTransportApplication.java
@@ -30,30 +30,13 @@ import com.venus.brTransport.service.UserService;
 
 @SpringBootApplication
 public class BrTransportApplication implements CommandLineRunner{
-	@Autowired
-	UserService userService;
-	
-	@Autowired
-	LieuService lieuService;
-	
-	@Autowired
-	OperationService operationService;
-	
-	@Autowired
-	MarchandiseService marchandiseService;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(BrTransportApplication.class, args);
 	}
 	@Override
 	public void run(String... args) throws Exception {
-		Document document = new Document();
-		PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
-		document.open();
-		Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-		Chunk chunk = new Chunk("Hello World", font);
-
-		document.add(chunk);
-		document.close();
 		
 	}
 }
